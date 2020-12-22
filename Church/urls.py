1b166c7af797dfc44a29281601a8342b4cb98092
andiwoedwin <andiwoedwin@gmail.com>
2020-12-22 10:41:39 +0300

    add urls

--- Church/urls.py
+++ Church/urls.py
@@ -1,12 +1,8 @@
 from django.urls import path
-from . import views
+from Church import views
 
 urlpatterns = [
     path('',views.about, name='about'),
-<<<<<<< HEAD
     path('index',views.index, name='home'),
-
-=======
     path('contact/',views.contact, name='contact'),    
->>>>>>> ddf1a07f8bd89f8e8af5886d33ae8fb449e86f2b
 ]
