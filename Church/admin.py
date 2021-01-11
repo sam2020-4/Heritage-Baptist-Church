8157029ed242faba107bc4521428cde860e21bed
Alexotieno1717 <alexotieno900@gmail.com>
2021-01-11 11:11:54 +0300

    modified index, youth and children ministry  page

--- Church/admin.py
+++ Church/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Team
+from .models import Team, Sermon, Children, Youth
 
 # Register your models here.
 admin.site.register(Team)
+admin.site.register(Sermon)
+admin.site.register(Children)
+admin.site.register(Youth)
--- Church/urls.py
+++ Church/urls.py
@@ -5,10 +5,12 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('',views.about, name='about'),
-    path('index',views.index, name='home'),
+    path('',views.index, name='home'),
+    path('about/',views.about, name='about'),
     path('contact/',views.contact, name='contact'), 
     path('give/',views.give, name='give'), 
+    path('children/',views.children, name='children'),
+    path('youth/',views.youth, name='youth'),
 ]
 
 if settings.DEBUG:urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
